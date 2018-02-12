c2f19ce01ffd6c2a6666b1bd25564ca3e0de2b05
Dave Shawley <daveshawley@gmail.com>
2018-02-11 20:46:39 -0500

    internals: add initialize_logging.
    This function updates the class used by logging,.getLogger to one that
    ensures that the ``divak_request_id`` attribute is always present on
    LogRecord instances.  It also inserts a logging.Filter implementation on
    existing log handlers that makes the same guarantee.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,7 +1,9 @@
 import logging
+import unittest
 
 from tornado import testing, web
 import divak.api
+import divak.internals
 import tests.application
 
 
@@ -35,3 +37,93 @@ class ApiLoggerTests(testing.AsyncHTTPTestCase):
         self.assertNotIn('my.custom.logger', self.log_manager.loggerDict)
         self.fetch('/logger')
         self.assertIn('my.custom.logger', self.log_manager.loggerDict)
+
+
+class LogInitializationTests(unittest.TestCase):
+
+    def setUp(self):
+        super(LogInitializationTests, self).setUp()
+        logging.setLoggerClass(logging.Logger)
+        manager = logging.Logger.manager  # type: logging.Manager
+        manager.loggerDict.clear()
+        for handler in logging.root.handlers:
+            filters = []
+            for filter in handler.filters:
+                if not isinstance(filter,
+                                  divak.internals.DivakRequestIdFilter):
+                    filters.append(filter)
+            handler.filters[:] = filters[:]
+
+    def test_that_new_logger_class_is_inserted(self):
+        divak.internals.initialize_logging()
+        self.assertIsNot(logging.getLoggerClass(), logging.Logger)
+        self.assertTrue(issubclass(logging.getLoggerClass(), logging.Logger))
+
+    def test_that_existing_loggers_are_filtered(self):
+        # Install a logger before calling initialize_logging.  Having
+        # multiple parts in the name path ensures that the logging layer
+        # inserts place holder loggers that we need to account for during
+        # initialization.
+        logger = logging.getLogger('package.sub.name')
+        self.assertIs(logger.__class__, logging.Logger)
+        self.assertFalse(
+            logger_has_filter(logger, divak.internals.DivakRequestIdFilter))
+
+        divak.internals.initialize_logging()
+        self.assertTrue(
+            logger_has_filter(logger, divak.internals.DivakRequestIdFilter))
+
+    def test_that_filters_are_not_duplicated(self):
+        # verify that a filter is only added to each handler once
+        handler = logging.NullHandler()
+        handler.addFilter(logging.Filter())  # tests coverage
+        logging.getLogger('one').addHandler(handler)
+        logging.getLogger('two').addHandler(handler)
+        logging.getLogger().addHandler(handler)
+
+        divak.internals.initialize_logging()
+        handlers = set()
+        for logger in logging.Logger.manager.loggerDict.values():
+            if hasattr(logger, 'handlers'):
+                handlers.update(logger.handlers)
+        for handler in handlers:
+            filter_count = 0
+            for filter in handler.filters:
+                if isinstance(filter, divak.internals.DivakRequestIdFilter):
+                    filter_count += 1
+            self.assertEqual(filter_count, 1)
+
+    def test_that_calling_initialize_logging_is_idempotent(self):
+        divak.internals.initialize_logging()
+        divak.internals.initialize_logging()
+        for handler in logging.getLogger().handlers:
+            count = 0
+            for filter in handler.filters:
+                if isinstance(filter, divak.internals.DivakRequestIdFilter):
+                    count += 1
+            self.assertEqual(count, 1)
+
+
+def logger_has_filter(logger, filter_class):
+    """
+    Will `logger` execute a filter of `filter_class`?
+
+    :param logging.Logger logger: the logger to examine
+    :param class filter_class: the filter class to search for
+    :return: :data:`True` if the `logger` contains a `filter_class`
+        filter instance in it's chain
+    :rtype: bool
+
+    """
+    if isinstance(logger, logging.Logger):
+        for filter in logger.filters:
+            if isinstance(filter, filter_class):
+                return True
+        for handler in logger.handlers:
+            for filter in handler.filters:
+                if isinstance(filter, filter_class):
+                    return True
+        if logger.propagate and logger.parent:
+            if logger_has_filter(logger.parent, filter_class):
+                return True
+    return False
